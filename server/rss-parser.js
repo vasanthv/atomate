0002f755119bfb3e6bc0d7ec9684e19fa67c5f86
vasanth <imvasanthv@gmail.com>
2024-02-13 20:52:49 -0500

    Created a RSS parser

---
+++ server/rss-parser.js
@@ -0,0 +1,87 @@
+/**
+ * This file is taken from https://github.com/nasa8x/rss-to-json/blob/master/src/parse.ts
+ * with some modifications as it will evolve in the future and we need control over this parser
+ * to iterate faster.
+ */
+const { XMLParser } = require("fast-xml-parser");
+const axios = require("axios");
+
+const rssParser = async (url) => {
+	const { data } = await axios(url);
+
+	const xml = new XMLParser({
+		attributeNamePrefix: "",
+		textNodeName: "$text",
+		ignoreAttributes: false,
+	});
+
+	const result = xml.parse(data);
+
+	let channel = result.rss?.channel ? result.rss.channel : result.feed;
+	if (Array.isArray(channel)) channel = channel[0];
+
+	let link = channel.link && channel.link.href ? channel.link.href : channel.link;
+	if (Array.isArray(link)) link = link[0].href ? link[0].href : link[0];
+
+	let items = channel.item || channel.entry || [];
+	if (items && !Array.isArray(items)) items = [items];
+
+	const rss = {
+		title: channel.title ?? "",
+		description: channel.description ?? "",
+		link,
+		items,
+		image: channel.image ? channel.image.url : channel["itunes:image"] ? channel["itunes:image"].href : "",
+	};
+
+	rss.items = rss.items.map((item) => {
+		let obj = {};
+		const media = {};
+
+		obj = {
+			id: item.guid && item.guid.$text ? item.guid.$text : item.id,
+			title: item.title && item.title.$text ? item.title.$text : item.title,
+			description: item.summary && item.summary.$text ? item.summary.$text : item.description,
+			link: item.link && item.link.href ? item.link.href : item.link,
+			author: item.author && item.author.name ? item.author.name : item["dc:creator"],
+			published: item.created
+				? new Date(item.created)
+				: item.pubDate
+				? new Date(item.pubDate)
+				: item.published
+				? new Date(item.published)
+				: undefined,
+			updated: item.updated ? new Date(item.updated) : undefined,
+			content: item.content && item.content.$text ? item.content.$text : item["content:encoded"],
+			enclosures: item.enclosure ? (Array.isArray(item.enclosure) ? item.enclosure : [item.enclosure]) : [],
+		};
+
+		if (item["media:thumbnail"]) {
+			Object.assign(media, { thumbnail: item["media:thumbnail"] });
+			obj.enclosures.push(item["media:thumbnail"]);
+		}
+
+		if (item["media:content"]) {
+			Object.assign(media, { thumbnail: item["media:content"] });
+			obj.enclosures.push(item["media:content"]);
+		}
+
+		if (item["media:group"]) {
+			if (item["media:group"]["media:title"]) obj.title = item["media:group"]["media:title"];
+
+			if (item["media:group"]["media:description"]) obj.description = item["media:group"]["media:description"];
+
+			if (item["media:group"]["media:thumbnail"]) obj.enclosures.push(item["media:group"]["media:thumbnail"].url);
+
+			if (item["media:group"]["media:content"]) obj.enclosures.push(item["media:group"]["media:content"]);
+		}
+
+		Object.assign(obj, { media });
+
+		return obj;
+	});
+
+	return rss;
+};
+
+module.exports = { rssParser };
